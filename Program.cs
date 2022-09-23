6313e7ec3c0e2aa4492a013ccdd5fbae7d08cf2c
yaroslav <95yzbek@gmail.com>
2022-09-23 20:06:15 +0500

    ex1

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿
+Console.Clear();
+Console.WriteLine("a=");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("b=");
+int b = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if (b >= max)
+{
+    Console.Write($"max = {b} min = {a}");
+}
+else
+{
+    Console.Write($"max = {a} min = {b}");
+}
